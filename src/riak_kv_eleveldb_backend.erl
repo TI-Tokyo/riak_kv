fe2a314fdca667442cc8bd14bd40c95973ad0b9e
Gordon Guthrie <gguthrie@basho.com>
2015-09-14 22:25:25 +0000

    Include file merge
    riak_ql_ddl.hrl and riak_ql_sql.hrl have been merged
    This change reflects that throught the code base

--- src/riak_kv_eleveldb_backend.erl
+++ src/riak_kv_eleveldb_backend.erl
@@ -55,7 +55,7 @@
                   ]}).
 
 -include("riak_kv_index.hrl").
--include_lib("riak_ql/include/riak_ql_sql.hrl").
+-include_lib("riak_ql/include/riak_ql_ddl.hrl").
 
 -ifdef(EQC).
 -include_lib("eqc/include/eqc.hrl").
