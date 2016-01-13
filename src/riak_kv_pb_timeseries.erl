f89bd7cc782063170f8dbb9357ebd2adbbab9695
Andrei Zavada <azavada@contractor.basho.com>
2016-01-13 02:28:33 +0200

    use defined record name when determining which query to run
    This properly belongs to 6233d586

--- src/riak_kv_pb_timeseries.erl
+++ src/riak_kv_pb_timeseries.erl
@@ -522,7 +522,7 @@ compile(Mod, {ok, SQL}) ->
 %%
 submit_query(DDL, SQL, State) ->
     case riak_kv_qry:submit(SQL, DDL) of
-        {ok, Data} when element(1, SQL) =:= riak_select_v1 ->
+        {ok, Data} when element(1, SQL) =:= ?SQL_SELECT_RECORD_NAME ->
             {reply, make_tsqueryresp(Data), State};
         {ok, Data} when element(1, SQL) =:= riak_sql_describe_v1 ->
             {reply, make_tsqueryresp(Data), State};
