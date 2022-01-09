322aab1b28e2db139bd1ebc0e0b40bd3648ebd1e
Andrei Zavada <andrei.zavada@tiot.jp>
2022-01-09 22:54:48 +0200

    complete riak_kv_qry:put, :submit works for full client put/select cycle
    * riak_kv_qry_worker: on receiving the first chunk from eleveldb,
      process and filter it and prepare for fetching; consume and discard
      the remaining chunks;
    * riak_kv_qry_worker: fast return on filtering data on '*';
    * riak_kv_pb_timeseries: properly implement put;
    * riak_kv_pb_timeseries: use riak_pb/riak_pb_ts_codec:encode_rows.
    
    # Conflicts:
    #	dialyzer.ignore-warnings
    #	src/riak_kv_qry_worker.erl

--- src/riak_kv_qry_worker.erl
+++ src/riak_kv_qry_worker.erl
@@ -68,7 +68,8 @@
           ddl        :: undefined | #ddl_v1{},
           qry = none :: none | #riak_sql_v1{},
           qid = undefined :: undefined | {node(), non_neg_integer()},
-          status = void   :: void | accumulating | complete,
+          n_chunks = 0    :: non_neg_integer(),
+          status = void   :: void | got_chunk | finished,
           result = []     :: [binary()]
          }).
 
@@ -127,29 +128,34 @@ handle_cast(Msg, State) ->
 -spec handle_info(term(), #state{}) -> {noreply, #state{}}.
 %% @private
 handle_info({QId, done},
-            State = #state{qid = QId}) ->
-    {noreply, State#state{status = complete}};
-
-handle_info({_QId, {results, [[]]}}, State) ->
-    lager:debug("Empty chunk on qid ~p", [_QId]),
-    {noreply, State};
-
-handle_info({QId, {results, Chunk}},
-            State = #state{qid = QId, qry = Qry,
-                           status = QStatus,
-                           result = Accumulated})
-  when is_list(Chunk) andalso (QStatus == void orelse QStatus == accumulating) ->
-    #riak_sql_v1{'SELECT' = SelectSpec} = Qry,
-    Decoded = decode_results(lists:flatten(Chunk), SelectSpec),
-    {noreply, State#state{status = accumulating,
-                          result = [Decoded | Accumulated]}};
-
-%% what if some late chunks arrive?
-handle_info({QId, {results, LateData}},
             State = #state{qid = QId,
-                           status = complete}) ->
-    lager:debug(
-      "Discarding ~p results.", [length(lists:flatten(LateData)), QId]),
+                           n_chunks = NChunks}) ->
+    lager:debug("Received ~b chunks on QId ~p", [NChunks, QId]),
+    {noreply, State#state{status = finished}};
+
+handle_info({QId, {results, [Chunk]}},  %% point of bother
+            State = #state{qid = QId, qry = Qry,
+                           status = void}) ->
+    #riak_sql_v1{'SELECT' = SelectSpec} = Qry,
+    Decoded =
+        decode_results(
+          Chunk, SelectSpec),
+    {noreply, State#state{status = got_chunk,
+                          n_chunks = 1,
+                          result = lists:append(Decoded)}};
+
+%% extra N chunks where N == n_val, n_val > 0: discard
+handle_info({QId, {results, [[_ExtraChunk]]}},
+            State = #state{qid = QId,
+                           n_chunks = NChunks,
+                           status = got_chunk}) ->
+    {noreply, State#state{n_chunks = NChunks + 1}};
+
+%% late chunks: warn
+handle_info({QId, {results, [[_LateData]]}},
+            State = #state{qid = QId,
+                           status = finished}) ->
+    lager:debug("Discarding late chunk (~b bytes) on qid ~p", [size(_LateData), QId]),
     {noreply, State};
 
 %% other error conditions
@@ -186,9 +192,7 @@ handle_req({execute, {QId, Qry, DDL}}, State = #state{status = void}) ->
     %% TODO make this run with multiple sub-queries
     Queries = riak_kv_qry_compiler:compile(DDL, Qry),
     SEs = [{run_sub_query, {qry, X}, {qid, QId}} || X <- Queries],
-    {ok, SEs, State#state{qid = QId, qry = Qry, ddl = DDL,
-                          status = accumulating,
-                          result = []}};
+    {ok, SEs, State#state{qid = QId, qry = Qry, ddl = DDL}};
 
 handle_req({execute, {QId, _, _}}, State = #state{status = Status})
   when Status =/= void ->
@@ -196,8 +200,14 @@ handle_req({execute, {QId, _, _}}, State = #state{status = Status})
     {{error, mismanagement}, [], State};
 
 handle_req({fetch, QId}, State = #state{qid = QId,
-                                        status = accumulating}) ->
+                                        status = void}) ->
     {{error, in_progress}, [], State};
+
+handle_req({fetch, QId}, State = #state{qid = QId,
+                                        status = got_chunk,
+                                        result = Result}) ->
+    %% perhaps we can just return the first chunk now but leave FSM to mop up the rest
+    {{ok, Result}, [], State};
 
 handle_req({fetch, QId}, State = #state{qid = QId,
                                         result = Result}) ->
@@ -224,16 +234,47 @@ handle_side_effects([H | T]) ->
     io:format("in riak_kv_qry:handle_side_effects not handling ~p~n", [H]),
     handle_side_effects(T).
 
-%%
-decode_results(KV_list, SelectSpec) ->
-    [extract_riak_object(SelectSpec, V) || {_,V} <- KV_list].
+decode_results(ListOfBins, SelectSpec) ->
+    decode_results(ListOfBins, SelectSpec, []).
+decode_results([], _SelectSpec, Acc) ->
+    %% lists:reverse(Acc);  %% recall that ListOfBins is in fact
+    %% reversed (as it was accumulated from chunks in handle_info);
+    Acc;
+decode_results([BList|Rest], SelectSpec, Acc) ->
+    Records = extract(BList, SelectSpec, []),
+    decode_results(
+      Rest, SelectSpec, [Records | Acc]).
 
-%%
-extract_riak_object(SelectSpec, V) when is_binary(V) ->
-    % don't care about bkey
-    RObj = riak_object:from_binary(<<>>, <<>>, V),
-    FullRecord = riak_object:get_value(RObj),
-    filter_columns(lists:flatten(SelectSpec), FullRecord).
+
+extract(<<>>, _SelectSpec, Acc) ->
+    Acc;
+extract(Batch, SelectSpec, Acc) ->
+    {_Key, B1} = eleveldb:parse_string(Batch),
+    { Val, B2} = eleveldb:parse_string(B1),
+    FullRecord =
+        eleveldb_ts:decode_record(
+          riak_object:get_value(
+            riak_object:from_binary(
+              %% don't care about bkey
+              <<>>, <<>>, Val, msgpack))),
+    Filtered =
+        filter_columns(
+          SelectSpec, FullRecord),
+    extract(B2, SelectSpec, [Filtered | Acc]).
+
+
+filter_columns([[<<"*">>]], KVList) ->
+    KVList;
+filter_columns(SelectSpec, KVList) ->
+    %% TODO: deal with operators and combinators
+    OnlyColumns = lists:foldl(
+                    fun([C], Acc) -> [binary_to_list(C)|Acc];
+                       (_, Acc) -> Acc
+                    end,
+                    [], SelectSpec),
+    lists:filter(
+      fun({Field, _Val}) -> lists:member(Field, OnlyColumns) end,
+      KVList).
 
 %% Pull out the values we're interested in based on the select,
 %% statement, e.g. select user, geoloc returns only user and geoloc columns.
