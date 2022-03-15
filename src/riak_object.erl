9662aea9831c2fb0fc7d63f716c0d7d044a1566c
Andrei Zavada <johnhommer@gmail.com>
2022-03-15 15:29:34 +0200

    fix msgpack:pack/2 options

--- src/riak_object.erl
+++ src/riak_object.erl
@@ -1629,12 +1629,12 @@ decode_maybe_binary(<<_Other:8, Bin/binary>>) ->
 sub_encode(Bin, erlang) ->
     <<?ERLT2B_MAGIC:8/integer, (term_to_binary(Bin))/binary>>;
 sub_encode(Bin, msgpack) ->
-    <<?MSGPACK_MAGIC:8/integer, (msgpack:pack(Bin, [{format, jsx}]))/binary>>.
+    <<?MSGPACK_MAGIC:8/integer, (msgpack:pack(Bin, [{spec, old}]))/binary>>.
 
 sub_decode(<<?ERLT2B_MAGIC:8/integer, Bin/binary>>) ->
     binary_to_term(Bin);
 sub_decode(<<?MSGPACK_MAGIC:8/integer, Bin/binary>>) ->
-    {ok, Unpacked} = msgpack:unpack(Bin, [{format, jsx}]),
+    {ok, Unpacked} = msgpack:unpack(Bin, [{spec, old}]),
     Unpacked;
 sub_decode(Bin) ->
     try
