97e11b8d9ce6a70363de92c3a54e723ddd72b130
Ted Burghart <tburghart@users.noreply.github.com>
2026-02-11 08:37:54 -0500

    General tune-up of reporting from the riak_kv_env module.
    * Adds references to where system limits are configured.
    * Adds comments in the source explaining limit thresholds.
    * Adjusts some thresholds to [hopefully] be more realistic.

--- src/riak_kv_env.erl
+++ src/riak_kv_env.erl
@@ -3,7 +3,7 @@
 %%
 %% Copyright (c) 2013-2014 Basho Technologies, Inc.
 %% Copyright (c) 2025 Workday, Inc.
-%% Copyright (c) 2026 T.R. Burghart.
+%% Copyright (c) 2026 The OpenRiak Project.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
@@ -113,11 +113,12 @@ doc_env() ->
 %% Internal
 %% ===================================================================
 
+-type conf_key()    :: nonempty_string().
+-type conf_value()  :: non_neg_integer() | atom().
 -type erts_desc()   :: nonempty_string().
 -type erts_key()    :: atom().
--type erts_param()  :: {erts_desc(), erts_key(), erts_value()}.
+-type erts_param()  :: {erts_key(), conf_value(), erts_desc(), conf_key()}.
 -type erts_params() :: list(erts_param()).
--type erts_value()  :: non_neg_integer().
 -type exe_arg()     :: binary().
 -type exe_args()    :: list(exe_arg()).
 -type exe_path()    :: binary().
@@ -138,30 +139,97 @@ doc_env() ->
 %%  max - Actual Value must be `<=' Target Value.
 %%  eq  - Actual Value must be `==' Target Value.
 
+%% 2^10 Multipliers
 -define(K(N),   (N * 1024)).
 -define(M(N),   ?K(N * 1024)).
+%% Milliseconds
 -define(MS(S),  (S * 1000)).
 
-%% TODO: Figure out recommended ?ERTS_GCGEN_* range
+%% erl +zdbbl - Distribution Buffer Busy limit
+%% Threshold here is in raw bytes, as set in Cuttlefish.
+%% https://www.erlang.org/doc/apps/erts/erl_cmd#+zdbbl
+%% https://www.erlang.org/doc/apps/erts/erlang#system_info_dist_buf_busy_limit
+%% Cuttlefish "erlang.distribution_buffer_size"
+-define(ERTS_DBUF_BUSY,     ?M(32)).
 
--define(ERTS_DBUF_BUSY,       ?M(32)).  % erl +zdbbl x 1024
--define(ERTS_ETS_MIN,         ?K(16)).  % erl +e, named ETS tables
--define(ERTS_GCGEN_MAX,        ?K(4)).  % erl -env ERL_FULLSWEEP_AFTER
--define(ERTS_GCGEN_MIN,           16).  % erl -env ERL_FULLSWEEP_AFTER
--define(ERTS_PORT_MIN,        ?K(64)).  % erl +Q, power of 2
--define(ERTS_PROC_MIN,       ?K(512)).  % erl +P, power of 2
--define(ERTS_SCHED_PCT,           95).  % Percentage schedulers/cores
--define(ERTS_THRD_MIN,             8).  % erl +A, mostly irrelevant
--define(OS_CORE_MIN,         ?K(512)).  % In blocks, generally 512 or 1024 bytes
--define(OS_FILE_MIN,          ?K(32)).  % Minimum open files
+%% erl +e - Maximum named ETS tables
+%% https://www.erlang.org/doc/apps/erts/erl_cmd#+e
+%% https://www.erlang.org/doc/apps/stdlib/ets#max_ets_tables
+%% Cuttlefish "erlang.max_ets_tables"
+%% This no longer sets a hard limit, instead serving as an optimization hint.
+%% If more than this many named ETS tables are present in the VM performance
+%% will be degraded.
+-define(ERTS_ETS_MIN,       ?K(16)).
+
+%% erl -env ERL_FULLSWEEP_AFTER
+%% Fullsweep GC generations interval
+%% https://www.erlang.org/doc/apps/erts/erlang#system_info_fullsweep_after
+%% https://www.erlang.org/doc/apps/erts/erlang#system_flag/2
+%% Cuttlefish "erlang.fullsweep_after"
+%% TODO: Figure out recommended fullsweep range
+%% The main consideration here are the long-lived VNode and backend processes
+%% and the binaries they handle, complicated by their distribution between
+%% process and binary heaps.
+%% The assumption is that the higher the fullsweep interval, the longer the
+%% actual GC will take when it's triggered, but until the issue is studied in
+%% depth we're only guessing.
+-define(ERTS_GCGEN_MAX,     ?K(16)).
+-define(ERTS_GCGEN_MIN,     16).
+
+%% erl +Q - Port Limit.
+%% Should be a power of 2.
+%% https://www.erlang.org/doc/apps/erts/erl_cmd#+Q
+%% https://www.erlang.org/doc/apps/erts/erlang#system_info_port_limit
+%% Cuttlefish "erlang.max_ports"
+-define(ERTS_PORT_MIN,      ?K(64)).
+
+%% erl +P - Process Limit.
+%% Should be a power of 2.
+%% https://www.erlang.org/doc/apps/erts/erl_cmd#+P
+%% https://www.erlang.org/doc/apps/erts/erlang#system_info_process_limit
+%% Cuttlefish "erlang.process_limit"
+-define(ERTS_PROC_MIN,      ?K(512)).
+
+%% Percentage schedulers/cores
+%% Cuttlefish "erlang.schedulers", "erlang.schedulers.*"
+-define(ERTS_SCHED_PCT,     95).
+
+%% erl +A - Async Thread Pool size.
+%% Mostly irrelevant, but wastes resources, log if higher than this threshold.
+%% https://www.erlang.org/doc/apps/erts/erl_cmd#+A
+%% https://www.erlang.org/doc/apps/erts/erlang#system_info_thread_pool_size
+%% Cuttlefish "erlang.async_threads"
+-define(ERTS_THRD_WARN,     16).
+
+%% erl +C - Time Warp Mode.
+%% https://www.erlang.org/doc/apps/erts/erl_cmd#+C
+%% https://www.erlang.org/doc/apps/erts/erlang#system_info_time_warp_mode
+%% Cuttlefish "erlang.time_warp_mode"
+-define(ERTS_TIME_MODE,     multi_time_warp).
+
+%% ulimit -c - Maximum Core Dump size.
+%% In blocks of 512 bytes on POSIX-compliant platforms, or 1024 bytes on Linux.
+-define(OS_CORE_MIN,        ?K(512)).
+
+%% ulimit -n - Maximum Open Files limit.
+%% Minimum OS open files limit.
+-define(OS_FILE_MIN,        ?K(64)).
 
 %% These will be logged in reverse order.
+%% Type: erts_params()
 -define(ERTS_LIMIT_PARAMS, [
-    {"ERTS dist buffer busy",   dist_buf_busy_limit,    ?ERTS_DBUF_BUSY},
-    {"ERTS thread pool",        thread_pool_size,       ?ERTS_THRD_MIN},
-    {"ERTS ETS table",          ets_limit,              ?ERTS_ETS_MIN},
-    {"ERTS ports",              port_limit,             ?ERTS_PORT_MIN},
-    {"ERTS process",            process_limit,          ?ERTS_PROC_MIN}
+    {time_warp_mode,        ?ERTS_TIME_MODE,
+        "ERTS time warp mode",      "erlang.time_warp_mode"},
+    {dist_buf_busy_limit,   ?ERTS_DBUF_BUSY,
+        "ERTS dist buffer busy",    "erlang.distribution_buffer_size"},
+    {thread_pool_size,      ?ERTS_THRD_WARN,
+        "ERTS thread pool",         "erlang.async_threads"},
+    {ets_limit,             ?ERTS_ETS_MIN,
+        "ERTS ETS table",           "erlang.max_ets_tables"},
+    {port_limit,            ?ERTS_PORT_MIN,
+        "ERTS ports",               "erlang.max_ports"},
+    {process_limit,         ?ERTS_PROC_MIN,
+        "ERTS process",             "erlang.process_limit"}
 ]).
 
 %% We assume all nodes are connected to a 1Gbps+ network with relatively low
@@ -210,9 +278,10 @@ doc_env() ->
     {<<"net.ipv4.tcp_max_syn_backlog">>,    ?SYS_NET_SYN_BACKLOG, min },
     {<<"net.ipv4.tcp_moderate_rcvbuf">>,                       1,  eq },
     {<<"net.ipv4.tcp_sack">>,                                  1,  eq },
+    {<<"net.ipv4.tcp_timestamps">>,                            1, min },
     {<<"net.ipv4.tcp_tw_reuse">>,                              1,  eq },
     {<<"net.ipv4.tcp_window_scaling">>,                        1,  eq },
-    {<<"vm.swappiness">>,                                      0, max }
+    {<<"vm.swappiness">>,                                     10, max }
 ]).
 
 %% See https://calomel.org/freebsd_network_tuning.html
@@ -252,8 +321,24 @@ erts_limits() ->
 
 -spec erts_limits(Params :: erts_params(), Result :: log_recs())
         -> log_recs().
-erts_limits([{Label, Key, Min} | Params], Result) ->
-    Rec = report_limit(Label, erlang:system_info(Key), Min),
+%% @hidden
+%% `thread_pool_size' gets special handling because we only want to warn if it's
+%% set to a high enough value that it might be wasting noticeable resources.
+erts_limits([{thread_pool_size = VmKey, Max, Label, CfKey} | Params], Result) ->
+    NextResult = case erlang:system_info(VmKey) of
+        Val when Val > Max ->
+            Rec = {notice,
+                "~ts limit of ~b is high, not more than ~b is recommended."
+                " Set with riak.conf: ~ts",
+                [Label, Val, Max, CfKey]},
+            [Rec | Result];
+        _ ->
+            Result
+    end,
+    erts_limits(Params, NextResult);
+erts_limits([{VmKey, Min, Label, CfKey} | Params], Result) ->
+    Rec = report_limit(Label,
+        "riak.conf: " ++ CfKey, erlang:system_info(VmKey), Min),
     erts_limits(Params, [Rec | Result]);
 erts_limits([], Result) ->
     Result.
@@ -275,7 +360,8 @@ erts_fullsweep_rec() ->
             {info, "ERTS fullsweep generations: ~b", [GCGens]};
         _ ->
             {notice,
-                "ERTS fullsweep generations of ~b is ~s, ~b-~b is recommended",
+                "ERTS fullsweep generations of ~b is ~s, ~b-~b is recommended."
+                " Set with riak.conf: erlang.fullsweep_after",
                 [GCGens, Relationship, ?ERTS_GCGEN_MIN, ?ERTS_GCGEN_MAX]}
     end.
 
@@ -288,17 +374,20 @@ erts_scheduler_rec() ->
             SchMin = (Cores * ?ERTS_SCHED_PCT),
             case (Scheds * 100) of
                 Sch when Sch >= SchMin ->
-                    {info, "Schedulers: ~b for ~b CPU cores", [Scheds, Cores]};
+                    {info, "ERTS Schedulers: ~b for ~b CPU cores", [Scheds, Cores]};
                 _ ->
                     {warning,
-                        "Running ~b schedulers for ~b CPU cores,"
-                        " at least ~b schedulers (~b%) recommended",
+                        "Running ~b ERTS schedulers for ~b CPU cores,"
+                        " at least ~b schedulers (~b%) recommended."
+                        " Set with riak.conf: erlang.schedulers[...]",
                         [Scheds, Cores, (SchMin div 100), ?ERTS_SCHED_PCT]}
             end;
         _ ->
             {notice,
-                "Running ~b schedulers for unknown CPU cores,"
-                " at least ~b% recommended", [Scheds, ?ERTS_SCHED_PCT]}
+                "Running ~b ERTS schedulers for unknown CPU cores,"
+                " at least ~b% of CPU cores recommended."
+                " Set with riak.conf: erlang.schedulers[...]",
+                [Scheds, ?ERTS_SCHED_PCT]}
     end.
 
 -spec os_limits(os_type()) -> log_recs().
@@ -359,7 +448,7 @@ u_limits(ULimitExe) ->
         unlimited ->
             {info, "Open files unlimited", []};
         _ ->
-            report_limit("Open files", OFLimit, ?OS_FILE_MIN)
+            report_limit("Open files", "ulimit -n", OFLimit, ?OS_FILE_MIN)
     end,
     CFLimit = case u_limit(ULimitExe, <<"-c">>) of
         unlimited ->
@@ -372,9 +461,10 @@ u_limits(ULimitExe) ->
             {info, "Core dump size unlimited", []};
         0 ->
             {notice,
-                "Core dumps are disabled, this may hinder debugging", []};
+                "Core dumps are disabled, this may hinder debugging."
+                " Enable with ulimit -c", []};
         _ ->
-            report_limit("Core dump size", CFLimit, ?OS_CORE_MIN)
+            report_limit("Core dump size", "ulimit -c", CFLimit, ?OS_CORE_MIN)
     end,
     [OFRec, CFRec].
 
@@ -406,17 +496,28 @@ u_limit_exec(Cmd, Args) ->
     end.
 
 -spec report_limit(
-    Label :: nonempty_string(), Val :: integer() | atom(), Min :: pos_integer())
+    Label :: nonempty_string(),
+    CfgKey :: conf_key(),
+    Val :: conf_value(),
+    Tgt :: conf_value() )
         -> log_rec().
 %% @hidden
-report_limit(Label, Val, Min) when erlang:is_integer(Val), Val < Min ->
+report_limit(Label, CfgKey, Val, Min) when erlang:is_integer(Val), Val < Min ->
     {warning,
-        "~ts limit of ~b is low, at least ~b is recommended",
-        [Label, Val, Min]};
-report_limit(Label, unknown, Min) ->
-    {notice, "~ts limit is unknown, at least ~b is recommended", [Label, Min]};
-report_limit(Label, Val, _Min) ->
-    {info, "~ts limit: ~0tp", [Label, Val]}.
+        "~ts limit of ~b is low, at least ~b is recommended. Set with ~ts",
+        [Label, Val, Min, CfgKey]};
+report_limit(Label, _CfgKey, Val, _Min) when erlang:is_integer(Val) ->
+    {info, "~ts limit: ~0tp", [Label, Val]};
+report_limit(Label, CfgKey, unknown, Min) ->
+    {notice,
+        "~ts limit is unknown, at least ~0tp is recommended. Set with ~ts",
+        [Label, Min, CfgKey]};
+report_limit(Label, _CfgKey, Val, Val) ->
+    {info, "~ts: ~0tp", [Label, Val]};
+report_limit(Label, CfgKey, Val, Tgt) ->
+    {warning,
+        "~ts is ~0tp, ~0tp is recommended. Set with ~ts",
+        [Label, Val, Tgt, CfgKey]}.
 
 -spec report_sysctl(
     SysctlExe :: exe_path(), ExeArg :: exe_arg(), Params :: sctl_params())
@@ -500,7 +601,7 @@ value_ok(min, Actual, Target) ->
 compare_text(eq) ->
     "equal to";
 compare_text(max) ->
-    "no more than";
+    "not more than";
 compare_text(min) ->
     "at least".
 
@@ -522,9 +623,9 @@ run_exe(Exe, Args) ->
     Mon = erlang:monitor(port, Port),
     Result = collect_output(Port, Mon, []),
     erlang:demonitor(Mon),
-    ?LOG_DEBUG("Time: ~bms to exec ~ts", [
+    ?LOG_DEBUG("Time: ~bµs to exec ~ts", [
         erlang:convert_time_unit((erlang:monotonic_time() - Start),
-            native, millisecond), lists:join(" ", [Exe | Args])]),
+            native, microsecond), lists:join(" ", [Exe | Args])]),
     Result.
 
 -spec collect_output(Port :: port(), Mon :: reference(), Result :: iolist())
